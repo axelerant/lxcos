d5aef4c37fb37e7de8c148a257cb09dda78db46c
Medhamsh V <medhamsh@axelerant.com>
2014-07-15 14:41:02 +0530

    typo fix

--- base/attributes/default.rb
+++ base/attributes/default.rb
@@ -3,5 +3,5 @@ g_start, g_range = ::File.read('/etc/subgid').scan(/goatos:(\d+):(\d+)/).flatten
 
 default['goatos']['user'] = 'goatos'
 default['goatos']['group'] = 'goatos'
-default['goatos']['subuid'] = <%= @u_start %> 
-default['goatos']['subgid'] = <%= @g_start %> 
+default['goatos']['subuid'] = '<%= @u_start %>'
+default['goatos']['subgid'] = '<%= @g_start %>' 
