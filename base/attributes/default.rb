ece75fe70bae05d57f64be141194f3e9c1d5be8d
Medhamsh V <medhamsh@axelerant.com>
2014-07-15 14:51:30 +0530

    removing subuid read trial from attributes

--- base/attributes/default.rb
+++ base/attributes/default.rb
@@ -1,7 +1,2 @@
-u_start, u_range = ::File.read('/etc/subuid').scan(/goatos:(\d+):(\d+)/).flatten
-g_start, g_range = ::File.read('/etc/subgid').scan(/goatos:(\d+):(\d+)/).flatten
-
 default['goatos']['user'] = 'goatos'
 default['goatos']['group'] = 'goatos'
-default['goatos']['subuid'] = '<%= @u_start %>'
-default['goatos']['subgid'] = '<%= @g_start %>' 
