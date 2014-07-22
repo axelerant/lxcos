719ddb883c1ed7e19f8fc327aba015272085afcc
Tapan Sharma <sharma.tapan27@gmail.com>
2014-07-22 18:28:38 +0530

    Removed rails app

--- ContainerBuilder/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ContainerBuilder::Application.config.secret_token = '44c267f563757b4bbab29b6ab50f0f8711421a1619e85bb68ede20062d4797299d055dbb2e06f60621204c4b405752de6565d03e0d9721d8433fb8de43caf952'
