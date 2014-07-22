074239ea065fe3fd107a6e0d89c9cd8206f0bfd6
Medhamsh V <medhamsh@axelerant.com>
2014-07-22 15:06:42 +0530

    separating commands of cgroups for sudo user and goatos user

--- base/recipes/configure.rb
+++ base/recipes/configure.rb
@@ -25,5 +25,13 @@ execute "combine-keys" do
 end
 
 execute "cgroups" do
-  command "sudo cgm create all goatos && sudo cgm chown all goatos $(id -u goatos) $(id -g goatos) && sudo -u goatos cgm movepid all goatos $$"
+  command "sudo cgm create all goatos && sudo cgm chown all goatos $(id -u goatos) $(id -g goatos)"
+end
+
+execute "movepids" do
+  cwd "/opt/goatos"
+  user "goatos"
+  action :run
+  environment ({'HOME' => '/opt/goatos', 'USER' => 'goatos'})
+  command "cgm movepid all goatos $$"
 end
